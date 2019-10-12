2148f1767d514a6ac85ce5ac49955e775d4d675d
afeef_zia <afeef.badri@gmail.com>
2019-10-12 10:03:53 +0200

    skip mesh with partitioning

--- src/skip-meshing-coarsining.cpp
+++ src/skip-meshing-coarsining.cpp
@@ -4,43 +4,85 @@
 using namespace std;
 
 
-int main()
-{
+int main(){
 
-double 	xx	,
-	yy	,
-	zz	;
+    int Scatter = 1 ;
 
-double xmin=873101.0000,	xmax=878099.0000	;
-double ymin=158000.0000,	ymax=1625999.0000	;
+    double x ;
+    double y ;
+    double z ;
 
-int pntx=2251;
-int pnty=2500;
+    int pntx = 2500 ;
+    int pnty = 2251 ;
 
-int skipx=25;
-int skipy=25;
+    int skipx = 499;
+    int skipy = 499;
+    ifstream in;
+        in.open("./../data/DEM_2m_new.xyz");
 
+//-----------------------------------------------------------------------------------//
+//---- Skipmeshing -----
+//-----------------------------------------------------------------------------------//
 
-ifstream in;
-in.open("./../data/DEM_2m_new.xyz");
+    if(Scatter!=1){
+    	ofstream wr;
+        wr.open("CoarseMesh-Skip500.xyz");
+        for(int j=0; j<pnty; j++){
+        for(int i=0; i<pntx; i++){
+	    in>>std::fixed>> x  >> y >> z;
 
-ofstream wr;
-wr.open("CoarseMesh-Skip100.xyz");
+	    if(int(j%skipy)==0 && int(i%skipx)==0 )
+	        wr<< std::fixed << x << "  " << y << "  "<<z << endl;
+        }
+        }
+    }
 
-double zznew;
+//-----------------------------------------------------------------------------------//
+//---- Skip mesh with partitioning -----
+//-----------------------------------------------------------------------------------//
 
+   if(Scatter==1){
 
-for(int i=0; i<pntx; i++){
-for(int j=0; j<pnty; j++){
-	in>>std::fixed>> xx  >> yy >> zz;
+        cout << " Please choose a Y-stripping number divisible or factor of :: " << int(pnty/skipy+1) << endl;
+ 
+        int Nscat = 3;
+        int countme=0, countme2=0;
+        cout << "\n\n";
+ 
+        cout << " *===================================================* \n" 
+             << " *                  IMPORTANT                        * \n"  
+             << " *===================================================* \n"        
+             << " *   Note that when using the parallel GFTM-mesher   * \n" 
+             << " *   please use << "<< Nscat <<" >> MPI processing units for     * \n"
+             << " *   optimal performance of the mesher in parallel   * \n"
+             << " *===================================================* \n"  
+             << 
+         endl;
 
-	if(int(j%skipy)==0 && int(i%skipx)==0 ){
-	wr<< std::fixed << xx << "  " << yy << "  "<<zz << endl;
-	}
-}
-}
+	int k=0;
+	ofstream wr;
+        wr.open("CoarseMesh-Skip500_"+std::to_string(k)+".xyz");
 
-cout << "Program ended successfully" << endl;
+        for(int j=0; j<pnty; j++){
+        for(int i=0; i<pntx; i++){
+
+	    in>>std::fixed>> x  >> y >> z;
+
+	    if(int(j%skipy)==0 && int(i%skipx)==0 ){
+                if(countme==int(int(pntx/skipx+1)*int(pnty/skipy+1))/Nscat){
+		countme=0; k++;
+    		wr.close();
+		wr.open("CoarseMesh-Skip500_"+std::to_string(k)+".xyz");
+		}
+	        wr<< std::fixed << x << "  " << y << "  "<<z << endl;
+	        countme++;
+            }
+        }
+        }
+
+    }
+
+    cout << "Program ended successfully" << endl;
 
 return 0;
 }
