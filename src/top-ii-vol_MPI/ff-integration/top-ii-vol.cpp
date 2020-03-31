9ce392e5f1c2eb5392d5fb0d1b0a40d62da2b813
MAB <afeef.badri@gmail.com>
2020-03-31 15:17:19 +0200

    split files to hpp

--- src/top-ii-vol_MPI/ff-integration/top-ii-vol.cpp
+++ src/top-ii-vol_MPI/ff-integration/top-ii-vol.cpp
@@ -1,11 +1,4 @@
-#include <iostream>
-#include <fstream>
-#include <iomanip>
-
 #include "ff++.hpp"
-#include "AFunction.hpp"
-#include "AFunction_ext.hpp"
-using namespace Fem2D;
 
 using namespace std;
 
@@ -86,17 +79,6 @@ if(mpirank == 0){
 }
 
 
-
-
-
-
-
-
-
-
-
-
-
 template<class K>
 class meshPointCloud_Op : public E_F0mps {
     public:
@@ -130,7 +112,8 @@ class meshPointCloud_Op : public E_F0mps {
 };
 
 template<class K>
-basicAC_F0::name_and_type meshPointCloud_Op<K>::name_param[] = { };
+basicAC_F0::name_and_type meshPointCloud_Op<K>::name_param[] = { 
+};
 
 
 template<class K>
@@ -160,248 +143,12 @@ AnyType meshPointCloud_Op<K>::operator()(Stack stack) const {
     int     pntz      = GetAny<long>((*ptz)(stack))	        ;
     double  zmax      = GetAny<double>((*dpth)(stack))	    ;
 
-//-----------------------------------------------------------------------------------//
-//---- Global Variables -----
-//-----------------------------------------------------------------------------------//
-
-    double xx	  ;
-    double yy	  ;
-    double zz	  ;
-    double delz	  ;
-    double zznew  ;
-
-    int IJK	  ;
-    int Ip1JK	  ;
-    int IJp1K	  ;
-    int IJKp1	  ;
-    int Ip1JKp1	  ;
-    int IJp1Kp1	  ;
-    int Ip1Jp1K	  ;
-    int Ip1Jp1Kp1 ;
-
     int pnty;
-    int pntx;  
+    int pntx; 
 
-
-//-----------------------------------------------------------------------------------//
-//---- Calculating Parameters -----
-//-----------------------------------------------------------------------------------//
-
-    ifstream in;
-    in.open(*inputfile+"_"+std::to_string(mpirank)+".xyz");
-
-    ifstream in1;
-    in1.open(*inputfile+"_"+std::to_string(mpirank)+".info");
-
-    ofstream wrgmsh;
-    wrgmsh.open(*outputfile+"_"+std::to_string(mpirank)+".mesh");
-
-    in1 >> pnty >> pntx;
-    in1.close() ;
-    
-
-    if(mpirank==(mpisize-1))pnty=pnty-1; // LAST rank does not have comman partion 
-
-
-    cout << "                                                               \n"
-         << "           MPI Processs # "<<mpirank <<"                          \n"
-         << "                                                               \n"
-         << "  # points in the .xyz ------------- "<< pntx*pnty << "        \n" 
-         << "  # points in X -------------------- "<< pntx << "             \n"
-         << "  # points in Y -------------------- "<< pnty << "             \n"
-         << "                                                               \n"
-         << " *============================================================*\n";
-
-//-----------------------------------------------------------------------------------//
-//---- Calculating Parameters -----
-//-----------------------------------------------------------------------------------//
-
-    int NPnt = pntx * pnty * pntz					;
-    int NTri = 4*((pntz-1)*(pntx-1)+(pnty-1)*(pntz-1)+(pntx-1)*(pnty-1));
-    int NTet = (pntx-1) * (pnty-1) * (pntz-1) * 6			;
-
-
-//-----------------------------------------------------------------------------------//
-//---- Header for mesh -----
-//-----------------------------------------------------------------------------------//
-
-    wrgmsh << "MeshVersionFormatted 1" 		<< endl;
-    wrgmsh << "" 				<< endl;
-    wrgmsh << "Dimension 3" 			<< endl;
-    wrgmsh << "" 				<< endl;
-
-//-----------------------------------------------------------------------------------//
-//---- Generating points -----
-//-----------------------------------------------------------------------------------//
-
-    cout   << "Generating points ........"		               ;
-    wrgmsh << "Vertices" 				<< endl;
-    wrgmsh <<  NPnt	 				<< endl;
-
-    for(int i=0; i<pntx*pnty; i++){
-        in     >> std::fixed >> xx >>        yy        >> zz                 ;
-        wrgmsh << std::fixed << xx << " " << yy << " " << zz << " 0"  << endl;
-
-        zznew=zz;  delz= (zmax-zz)/(pntz-1);
-        for(int j=0; j<pntz-1; j++){
-            zznew  = zznew + delz;
-            wrgmsh << std::fixed << xx << " " << yy << " " << zznew << " 0" << endl;
-        }
-    }
-
-    wrgmsh << "" 					<< endl;
-    cout   << " finished for MPI rank : " << mpirank << endl;
-
-//-----------------------------------------------------------------------------------//
-//---- Generating Tetrahedra -----
-//-----------------------------------------------------------------------------------//
-
-    cout   << "Generating Tetrahedra ...."			       ;
-    wrgmsh << "Tetrahedra" 					<< endl;
-    wrgmsh <<  NTet	        				<< endl;
-
-    for(int j=0; j<pnty-1;  j++){
-    for(int i=0; i<pntx-1;  i++){
-    for(int k=1; k<=pntz-1; k++){
-
-        IJK	    =	i*pntz  + j*pntx*pntz + k	;
-        Ip1JK	    =	IJK 	+ (pntx*pntz)		;
-        IJp1K	    =	IJK 	+ (pntz)		;
-        Ip1Jp1K   =	IJK 	+ (pntx*pntz) + pntz	;
-        IJKp1     =	IJK 	+ 1			;
-        Ip1JKp1   =	Ip1JK 	+ 1			;
-        IJp1Kp1   =	IJp1K   + 1			;
-        Ip1Jp1Kp1 =	Ip1Jp1K + 1			;
-
-        wrgmsh << std::fixed << IJK     << " " << IJKp1   << " " << IJp1K     << " " << Ip1Jp1K << " 0" << endl;
-        wrgmsh << std::fixed << IJKp1   << " " << IJK     << " " << Ip1JK     << " " << Ip1Jp1K << " 0" << endl;
-        wrgmsh << std::fixed << Ip1JKp1 << " " << IJKp1   << " " << Ip1JK     << " " << Ip1Jp1K << " 0" << endl;
-        wrgmsh << std::fixed << IJKp1   << " " << Ip1JKp1 << " " << Ip1Jp1Kp1 << " " << Ip1Jp1K << " 0" << endl;
-        wrgmsh << std::fixed << IJp1Kp1 << " " << IJKp1   << " " << Ip1Jp1Kp1 << " " << Ip1Jp1K << " 0" << endl;
-        wrgmsh << std::fixed << IJKp1   << " " << IJp1Kp1 << " " << IJp1K     << " " << Ip1Jp1K << " 0" << endl;
-    }
-    }
-    }
-
-    wrgmsh << "" 					    << endl;
-    cout   << " finished for MPI rank : " << mpirank << endl;
-
-//-----------------------------------------------------------------------------------//
-//---- Generating Triangles -----
-//-----------------------------------------------------------------------------------//
-
-    cout   << "Generating Triangles ....."			       ;
-    wrgmsh << "Triangles" 					<< endl;
-    wrgmsh << NTri	        				<< endl;
-
-//----X-MIN-PLANE---//
-
-    for(int i=0; i<pnty-1;  i++){
-    for(int j=0; j<pntz-1;  j++){
-
-        IJK	  =	i*(pntz*pntx) + j+1	;
-        IJKp1	  =	IJK + 1			;
-        Ip1JK	  =	IJK + (pntz*pntx)	;
-        Ip1JKp1 =	Ip1JK + 1		;
-
-        wrgmsh << std::fixed << IJKp1   << " " <<  IJK  << " " << Ip1JK << " 1" << endl;
-        wrgmsh << std::fixed << Ip1JKp1 << " " << IJKp1 << " " << Ip1JK << " 1" << endl;
-    }
-    }
-
-
-//----Y-MIN-PLANE----//
-int labymin=99099;
-
-if(mpirank==0)labymin=2;
-
-    for(int i=0; i<pntx-1;  i++){
-    for(int j=0; j<pntz-1;  j++){
-
-        IJK	  =	i*pntz + j+1	;
-        IJKp1	  =	IJK + 1		;
-        IJp1K	  =	IJK + pntz	;
-        IJp1Kp1 =	IJp1K + 1	;
-
-        wrgmsh << std::fixed << IJK   << " " << IJKp1   << " " << IJp1K << " " << labymin << endl;
-        wrgmsh << std::fixed << IJKp1 << " " << IJp1Kp1 << " " << IJp1K << " " << labymin << endl;
-
-    }
-    }
-
-
-//----Z-MIN-PLANE----//
-    for(int i=0; i<pnty-1;  i++){
-    for(int j=0; j<pntx-1;  j++){
-
-        IJK	  =	i*(pntz*pntx) + j*(pntz)+1	;
-        Ip1JK	  =	IJK + (pntz*pntx)		;
-        IJp1K	  =	IJK + pntz			;
-        Ip1Jp1K =	Ip1JK + pntz			;
-
-        wrgmsh << std::fixed << IJK   << " " << IJp1K << " " << Ip1Jp1K << " 3" << endl;
-        wrgmsh << std::fixed << Ip1JK << " " << IJK   << " " << Ip1Jp1K << " 3" << endl;
-    }
-    }
-
-//----X-MAX-PLANE----//
-    for(int i=0; i<pnty-1;  i++){
-    for(int j=0; j<pntz-1;  j++){
-
-        IJK	  =	i*(pntz*pntx) + j+1 + (pntx-1)*(pntz)	;
-        IJKp1	  =	IJK + 1					;
-        Ip1JK	  =	IJK + (pntz*pntx)			;
-        Ip1JKp1 =	Ip1JK + 1				;
-
-        wrgmsh << std::fixed << IJK   << " " << IJKp1   << " " << Ip1JK << " 4" << endl;
-        wrgmsh << std::fixed << IJKp1 << " " << Ip1JKp1 << " " << Ip1JK << " 4" << endl;
-
-
-    }
-    }
-
-
-//----Y-MAX-PLANE----//
-int labymax=99099;
-
-if(mpirank==(mpisize-1))labymax=5;
-
-    for(int i=0; i<pntx-1;  i++){
-    for(int j=0; j<pntz-1;  j++){
-
-        IJK	  =	i*pntz + j+1 + (pntx*pntz*(pnty-1))	;
-        IJKp1	  =	IJK + 1					;
-        IJp1K	  =	IJK + pntz				;
-        IJp1Kp1 =	IJp1K + 1				;
-
-        wrgmsh << std::fixed << IJKp1   << " " << IJK   << " " << IJp1K << " " << labymax << endl;
-        wrgmsh << std::fixed << IJp1Kp1 << " " << IJKp1 << " " << IJp1K << " " << labymax << endl;
-
-    }
-    }
-//}
-
-//----Z-MAX-PLANE----//
-    for(int i=0; i<pnty-1;  i++){
-    for(int j=0; j<pntx-1;  j++){
-
-        IJK	  =	i*(pntz*pntx) + j*(pntz)+1 + (pntz-1)	;
-        Ip1JK	  =	IJK + (pntz*pntx)			;
-        IJp1K	  =	IJK + pntz				;
-        Ip1Jp1K =	Ip1JK + pntz				;
-
-        wrgmsh << std::fixed << IJp1K << " " << IJK   << " " << Ip1Jp1K << " 6" << endl;
-        wrgmsh << std::fixed << IJK   << " " << Ip1JK << " " << Ip1Jp1K << " 6" << endl;
-    }
-    }
-
-    cout   << " finished for MPI rank : " << mpirank << endl;
-
-//-----------------------------------------------------------------------------------//
-//---- Finishing footer -----
-//-----------------------------------------------------------------------------------//
-    wrgmsh << "" 					        << endl;
-    wrgmsh << "End" 						<< endl;  
+    if(mpirank==0)
+     #include "./../LogoTopiiVolCpp.hpp"
+     #include "./../TopiiVolMeshAlgo.hpp"
 };
 
 static void InitFF()
--- src/top-ii-vol_MPI/top-ii-vol-MPImesher.cpp
+++ src/top-ii-vol_MPI/top-ii-vol-MPImesher.cpp
@@ -8,39 +8,37 @@ using namespace std;
 
 int main(int argc, char **argv){
 
-    MPI_Init(&argc, &argv);
+//-----------------------------------------------------------------------------------//
+//---- MPI variables -----
+//-----------------------------------------------------------------------------------//
 
+    MPI_Init(&argc, &argv);
     int mpisize, mpirank;
     MPI_Comm_size(MPI_COMM_WORLD, &mpisize);
     MPI_Comm_rank(MPI_COMM_WORLD, &mpirank);
 
+//-----------------------------------------------------------------------------------//
+//---- Logo -----
+//-----------------------------------------------------------------------------------//
+
+    if(mpirank==0)
+     #include "LogoTopiiVolCpp.hpp"
+
+//-----------------------------------------------------------------------------------//
+//---- Time variable -----
+//-----------------------------------------------------------------------------------//
+
     double t1 = MPI_Wtime();
-
-//-----------------------------------------------------------------------------------//
-//---- Global Variables -----
-//-----------------------------------------------------------------------------------//
-
-    double xx	  ;
-    double yy	  ;
-    double zz	  ;
-    double delz	  ;
-    double zznew  ;
-
-    int IJK	  ;
-    int Ip1JK	  ;
-    int IJp1K	  ;
-    int IJKp1	  ;
-    int Ip1JKp1	  ;
-    int IJp1Kp1	  ;
-    int Ip1Jp1K	  ;
-    int Ip1Jp1Kp1 ;
 
 //-----------------------------------------------------------------------------------//
 //---- I/O Files -----
 //-----------------------------------------------------------------------------------//
 
-    string inputfile  = "point-cloud-strip";
-    string outputfile = "Top2Vol-mesh";
+    string * inputfile  = new string();
+    string * outputfile = new string();
+
+    * inputfile  = "point-cloud-strip";
+    * outputfile = "Top2Vol-mesh";
 
 //-----------------------------------------------------------------------------------//
 //---- Input Parameters -----
@@ -51,6 +49,7 @@ int main(int argc, char **argv){
     int	pntz=9;
     int pnty=6;
     int pntx=500;  //250;//125;//63;//32;
+
 
 //-----------------------------------------------------------------------------------//
 //---- Comandline Parameters -----
@@ -64,257 +63,25 @@ int main(int argc, char **argv){
 	    	pntz = atoi(argv[i+1]);
 
 		if ( ! strcmp(argv[i], "--in"))
-            inputfile  = argv[i+1];
-			//strcpy(inputfile, argv[i+1]); 
+            *inputfile  = argv[i+1];
 
 		if ( ! strcmp(argv[i], "--out"))
-            outputfile  = argv[i+1];
-			//strcpy(outputfile, argv[i+1]); 
+            *outputfile  = argv[i+1];
 
 		if ( ! strcmp(argv[i], "--depth")) 
 	    	zmax= atol(argv[i+1]);	
      }
 
-
 //-----------------------------------------------------------------------------------//
-//---- Calculating Parameters -----
-//-----------------------------------------------------------------------------------//
-
-    ifstream in;
-    in.open(inputfile+"_"+std::to_string(mpirank)+".xyz");
-
-    ifstream in1;
-    in1.open(inputfile+"_"+std::to_string(mpirank)+".info");
-
-    ofstream wrgmsh;
-    wrgmsh.open(outputfile+"_"+std::to_string(mpirank)+".mesh");
-
-    in1 >> pnty >> pntx;
-    in1.close() ;
-    
-
-    if(mpirank==(mpisize-1))pnty=pnty-1; // Last mpirank does not have comman partion 
-
-//-----------------------------------------------------------------------------------//
-//---- Calculating Parameters -----
+//---- Main algo -----
 //-----------------------------------------------------------------------------------//
 
-    int NPnt = pntx * pnty * pntz					;
-    int NTri = 4*((pntz-1)*(pntx-1)+(pnty-1)*(pntz-1)+(pntx-1)*(pnty-1));
-    int NTet = (pntx-1) * (pnty-1) * (pntz-1) * 6			;
+     #include "TopiiVolMeshAlgo.hpp"
 
-
-//=============================================================================
-// ------------- Commandline logo output ------------------
-//=============================================================================
-
-    if(mpirank==0)
-    cout << " *============================================================*\n" 
-         << "         ___                                            ___    \n" 
-         << "        /  /               ___________                 /  /    \n" 
-         << "     __/  /_ ___    ___   /__  __  __/__    __ ____   /  /     \n" 
-         << "    /_   __// _  \\ / _  \\   / / / /   \\ \\  / // _  \\ /  / \n" 
-         << "     /  /_ / /_/ // /_/ /__/ /_/ /__   \\ \\/ // /_/ //  /__   \n" 
-         << "     \\___/ \\____// ____//__________/    \\__/ \\____/ \\____/\n" 
-         << "                / /                                            \n" 
-         << "               /_/                                             \n"
-         << " *============================================================*\n"; 
-
-    MPI_Barrier(MPI_COMM_WORLD);
-
-    cout << "                                                               \n"
-         << "           MPI Processs # "<<mpirank <<"                       \n"
-         << "                                                               \n"
-         << "  # points in the .xyz ------------- "<< pntx*pnty << "        \n" 
-         << "  # points in X -------------------- "<< pntx << "             \n"
-         << "  # points in Y -------------------- "<< pnty << "             \n"
-         << "                                                               \n"
-         << " *============================================================*\n";
 
 //-----------------------------------------------------------------------------------//
-//---- Header for mesh -----
+//---- Time  -----
 //-----------------------------------------------------------------------------------//
-
-    wrgmsh << "MeshVersionFormatted 1" 		<< endl;
-    wrgmsh << "" 				<< endl;
-    wrgmsh << "Dimension 3" 			<< endl;
-    wrgmsh << "" 				<< endl;
-
-//-----------------------------------------------------------------------------------//
-//---- Generating points -----
-//-----------------------------------------------------------------------------------//
-
-    cout   << "Generating points ........"		               ;
-    wrgmsh << "Vertices" 		    << endl;
-    wrgmsh <<  NPnt	 				<< endl;
-
-    for(int i=0; i<pntx*pnty; i++){
-        in     >> std::fixed >> xx >>        yy        >> zz                 ;
-        wrgmsh << std::fixed << xx << " " << yy << " " << zz << " 0"  << endl;
-
-        zznew=zz;  delz= (zmax-zz)/(pntz-1);
-        for(int j=0; j<pntz-1; j++){
-            zznew  = zznew + delz;
-            wrgmsh << std::fixed << xx << " " << yy << " " << zznew << " 0" << endl;
-        }
-    }
-
-    wrgmsh << "							" << endl;
-    cout   << " finished for MPI rank : " << mpirank << endl;
-
-//-----------------------------------------------------------------------------------//
-//---- Generating Tetrahedra -----
-//-----------------------------------------------------------------------------------//
-
-    cout   << "Generating Tetrahedra ...."			       ;
-    wrgmsh << "Tetrahedra" 					<< endl;
-    wrgmsh <<  NTet	        				<< endl;
-
-    for(int j=0; j<pnty-1;  j++){
-    for(int i=0; i<pntx-1;  i++){
-    for(int k=1; k<=pntz-1; k++){
-
-        IJK	    =	i*pntz  + j*pntx*pntz + k	;
-        Ip1JK	    =	IJK 	+ (pntx*pntz)		;
-        IJp1K	    =	IJK 	+ (pntz)		;
-        Ip1Jp1K   =	IJK 	+ (pntx*pntz) + pntz	;
-        IJKp1     =	IJK 	+ 1			;
-        Ip1JKp1   =	Ip1JK 	+ 1			;
-        IJp1Kp1   =	IJp1K   + 1			;
-        Ip1Jp1Kp1 =	Ip1Jp1K + 1			;
-
-        wrgmsh << std::fixed << IJK     << " " << IJKp1   << " " << IJp1K     << " " << Ip1Jp1K << " 0" << endl;
-        wrgmsh << std::fixed << IJKp1   << " " << IJK     << " " << Ip1JK     << " " << Ip1Jp1K << " 0" << endl;
-        wrgmsh << std::fixed << Ip1JKp1 << " " << IJKp1   << " " << Ip1JK     << " " << Ip1Jp1K << " 0" << endl;
-        wrgmsh << std::fixed << IJKp1   << " " << Ip1JKp1 << " " << Ip1Jp1Kp1 << " " << Ip1Jp1K << " 0" << endl;
-        wrgmsh << std::fixed << IJp1Kp1 << " " << IJKp1   << " " << Ip1Jp1Kp1 << " " << Ip1Jp1K << " 0" << endl;
-        wrgmsh << std::fixed << IJKp1   << " " << IJp1Kp1 << " " << IJp1K     << " " << Ip1Jp1K << " 0" << endl;
-    }
-    }
-    }
-
-    wrgmsh << "" 					      << endl;
-    cout   << " finished for MPI rank : " << mpirank << endl;
-
-//-----------------------------------------------------------------------------------//
-//---- Generating Triangles -----
-//-----------------------------------------------------------------------------------//
-
-    cout   << "Generating Triangles ....."			       ;
-    wrgmsh << "Triangles" 					<< endl;
-    wrgmsh << NTri	        				<< endl;
-
-//----X-MIN-PLANE---//
-
-    for(int i=0; i<pnty-1;  i++){
-    for(int j=0; j<pntz-1;  j++){
-
-        IJK	  =	i*(pntz*pntx) + j+1	;
-        IJKp1	  =	IJK + 1			;
-        Ip1JK	  =	IJK + (pntz*pntx)	;
-        Ip1JKp1 =	Ip1JK + 1		;
-
-        wrgmsh << std::fixed << IJKp1   << " " <<  IJK  << " " << Ip1JK << " 1" << endl;
-        wrgmsh << std::fixed << Ip1JKp1 << " " << IJKp1 << " " << Ip1JK << " 1" << endl;
-    }
-    }
-
-
-//----Y-MIN-PLANE----//
-int labymin=99099;
-
-if(mpirank==0)labymin=2;
-
-    for(int i=0; i<pntx-1;  i++){
-    for(int j=0; j<pntz-1;  j++){
-
-        IJK	  =	i*pntz + j+1	;
-        IJKp1	  =	IJK + 1		;
-        IJp1K	  =	IJK + pntz	;
-        IJp1Kp1 =	IJp1K + 1	;
-
-        wrgmsh << std::fixed << IJK   << " " << IJKp1   << " " << IJp1K << " " << labymin << endl;
-        wrgmsh << std::fixed << IJKp1 << " " << IJp1Kp1 << " " << IJp1K << " " << labymin << endl;
-
-    }
-    }
-
-
-//----Z-MIN-PLANE----//
-    for(int i=0; i<pnty-1;  i++){
-    for(int j=0; j<pntx-1;  j++){
-
-        IJK	  =	i*(pntz*pntx) + j*(pntz)+1	;
-        Ip1JK	  =	IJK + (pntz*pntx)		;
-        IJp1K	  =	IJK + pntz			;
-        Ip1Jp1K =	Ip1JK + pntz			;
-
-        wrgmsh << std::fixed << IJK   << " " << IJp1K << " " << Ip1Jp1K << " 3" << endl;
-        wrgmsh << std::fixed << Ip1JK << " " << IJK   << " " << Ip1Jp1K << " 3" << endl;
-    }
-    }
-
-//----X-MAX-PLANE----//
-    for(int i=0; i<pnty-1;  i++){
-    for(int j=0; j<pntz-1;  j++){
-
-        IJK	  =	i*(pntz*pntx) + j+1 + (pntx-1)*(pntz)	;
-        IJKp1	  =	IJK + 1					;
-        Ip1JK	  =	IJK + (pntz*pntx)			;
-        Ip1JKp1 =	Ip1JK + 1				;
-
-        wrgmsh << std::fixed << IJK   << " " << IJKp1   << " " << Ip1JK << " 4" << endl;
-        wrgmsh << std::fixed << IJKp1 << " " << Ip1JKp1 << " " << Ip1JK << " 4" << endl;
-
-
-    }
-    }
-
-
-//----Y-MAX-PLANE----//
-int labymax=99099;
-
-if(mpirank==(mpisize-1))labymax=5;
-
-    for(int i=0; i<pntx-1;  i++){
-    for(int j=0; j<pntz-1;  j++){
-
-        IJK	  =	i*pntz + j+1 + (pntx*pntz*(pnty-1))	;
-        IJKp1	  =	IJK + 1					;
-        IJp1K	  =	IJK + pntz				;
-        IJp1Kp1 =	IJp1K + 1				;
-
-        wrgmsh << std::fixed << IJKp1   << " " << IJK   << " " << IJp1K << " " << labymax << endl;
-        wrgmsh << std::fixed << IJp1Kp1 << " " << IJKp1 << " " << IJp1K << " " << labymax << endl;
-
-    }
-    }
-//}
-
-//----Z-MAX-PLANE----//
-    for(int i=0; i<pnty-1;  i++){
-    for(int j=0; j<pntx-1;  j++){
-
-        IJK	  =	i*(pntz*pntx) + j*(pntz)+1 + (pntz-1)	;
-        Ip1JK	  =	IJK + (pntz*pntx)			;
-        IJp1K	  =	IJK + pntz				;
-        Ip1Jp1K =	Ip1JK + pntz				;
-
-        wrgmsh << std::fixed << IJp1K << " " << IJK   << " " << Ip1Jp1K << " 6" << endl;
-        wrgmsh << std::fixed << IJK   << " " << Ip1JK << " " << Ip1Jp1K << " 6" << endl;
-    }
-    }
-
-    cout   << " finished for MPI rank : " << mpirank << endl;
-
-//-----------------------------------------------------------------------------------//
-//---- Finishing footer -----
-//-----------------------------------------------------------------------------------//
-
-    wrgmsh << "" 					        << endl;
-    wrgmsh << "End" 						<< endl;
-
-
 
    MPI_Barrier(MPI_COMM_WORLD);
    if(mpirank==0){
@@ -327,6 +94,9 @@ if(mpirank==(mpisize-1))labymax=5;
          << " *============================================================*\n"
          << "                                                               \n";
     }
+
+
     MPI_Finalize();
+
 return 0;
 }
