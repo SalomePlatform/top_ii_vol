7a4114097512c8f5e78d38658e4bfe66a47651b9
MAB <afeef.badri@gmail.com>
2020-04-15 21:58:26 +0200

    added header

--- src/mesher/Top2Vol-preproc.cpp
+++ src/mesher/Top2Vol-preproc.cpp
@@ -1,6 +1,27 @@
+/***********************************************************************
+
+     This file is a part of top-ii-vol meshing tools.
+
+     -------------------------------------------------------------------
+
+     Author(s): Mohd Afeef Badri 
+     Email    : mohd-afeef.badri@hotmail.fr
+     Date     : 2019‑12‑09
+
+     -------------------------------------------------------------------
+
+     top-ii-vol  provides  sequential  and  parallel tools for  creating  
+     volumic tetrahedral meshes from a topology defined by apoint cloud.
+     top-ii-vol  is  distributed in the hope that it will be useful, but 
+     WITHOUT  ANY  WARRANTY; or  without  even  the  implied warranty of 
+     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+************************************************************************/
+
 #include <iostream>
 #include <fstream>
 #include <iomanip>
+
 using namespace std;
 
 
