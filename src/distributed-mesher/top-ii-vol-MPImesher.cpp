fcf6249ebee61ba64573c495b46411eec007e228
MAB <afeef.badri@gmail.com>
2020-04-16 16:12:18 +0200

    Dist mesh in one go

--- src/distributed-mesher/top-ii-vol-MPImesher.cpp
+++ src/distributed-mesher/top-ii-vol-MPImesher.cpp
@@ -19,13 +19,13 @@
 *******************************************************************************/
 
 
-#include <iostream>
-#include <fstream>
-#include <string>
-#include <iomanip>
-#include <mpi.h>
-using namespace std;
+ #include <iostream>
+ #include <fstream>
+ #include <string>
+ #include <iomanip>
+ #include <mpi.h>
 
+ using namespace std;
 
 int main(int argc, char **argv){
 
@@ -33,89 +33,125 @@ int main(int argc, char **argv){
 //---- MPI variables -----
 //-----------------------------------------------------------------------------------//
 
-    MPI_Init(&argc, &argv);
-    int mpisize, mpirank;
-    MPI_Comm_size(MPI_COMM_WORLD, &mpisize);
-    MPI_Comm_rank(MPI_COMM_WORLD, &mpirank);
+ int mpisize;
+ int mpirank;
+ 
+ MPI_Init(&argc, &argv);
+ MPI_Comm_size(MPI_COMM_WORLD, &mpisize);
+ MPI_Comm_rank(MPI_COMM_WORLD, &mpirank);
 
 //-----------------------------------------------------------------------------------//
 //---- Logo -----
 //-----------------------------------------------------------------------------------//
 
-    if(mpirank==0)
-     #include "./../lib/LogoTopiiVolCpp.hpp"
+ if(mpirank==0)
+   #include "./../lib/LogoTopiiVolCpp.hpp"
 
 //-----------------------------------------------------------------------------------//
 //---- I/O Files -----
 //-----------------------------------------------------------------------------------//
 
-    string * inputfile  = new string();
-    string * outputfile = new string();
+ string *inputfile  = new string();
+ string *outputfile = new string();
 
-    * inputfile  = "point-cloud-strip";
-    * outputfile = "Top2Vol-mesh";
+ *inputfile  = "./../../data/DEM_160m";
+ *outputfile = "point-cloud-strip"; 
 
 //-----------------------------------------------------------------------------------//
 //---- Input Parameters -----
 //-----------------------------------------------------------------------------------//
 
-    double zmax = -1920.0  ;
+//-----------------------------------------//
+//---- Points in z and zmax -----
+//-----------------------------------------// 
+    
+ double zmax = -1920.0  ;
+ int	pntz = 9        ;
+       
+//-----------------------------------------//
+//---- Points in x & y on the surface -----
+//-----------------------------------------// 
 
-    int	pntz=9;
-    int pnty=6;
-    int pntx=500;  //250;//125;//63;//32;
-
+                // 10m  20m  40m  80m 160m      
+ int pntx = 32; // 500; 250; 125; 63; 32;
+ int pnty = 29; // 451; 226; 113; 57; 29;
 
 //-----------------------------------------------------------------------------------//
 //---- Comandline Parameters -----
 //-----------------------------------------------------------------------------------//
 
-    for(int i=0; i<argc-1; i++){
+ for(int i=0; i<argc-1; i++){
 
-        string argvdummy=argv[i];
+   string argvdummy = argv[i];
+   
+   if(!strcmp(argv[i], "--xpoints")) 
+     pntx = atoi(argv[i+1]);
 
-		if ( ! strcmp(argv[i], "--zpoints")) 
-	    	pntz = atoi(argv[i+1]);
+   if(!strcmp(argv[i], "--ypoints")) 
+     pnty = atoi(argv[i+1]);	    	
 
-		if ( ! strcmp(argv[i], "--in"))
-            *inputfile  = argv[i+1];
+   if(!strcmp(argv[i], "--zpoints")) 
+     pntz = atoi(argv[i+1]);
 
-		if ( ! strcmp(argv[i], "--out"))
-            *outputfile  = argv[i+1];
+   if(!strcmp(argv[i], "--in"))
+     *inputfile  = argv[i+1];
 
-		if ( ! strcmp(argv[i], "--depth")) 
-	    	zmax= atol(argv[i+1]);	
-     }
+   if(!strcmp(argv[i], "--depth")) 
+     zmax= atol(argv[i+1]);	
+ }
 
 //-----------------------------------------------------------------------------------//
 //---- Time variable -----
 //-----------------------------------------------------------------------------------//
 
-    double t1 = MPI_Wtime();
+ double t1 = MPI_Wtime();
+    
+//-----------------------------------------------------------------------------------//
+//---- Main algo for partitioning -----
+//-------------------------------------------------------------------------------
+
+ if(mpirank==0){
+   #include "./../lib/TopiiVolPartAlgo.hpp"
+ }
 
 //-----------------------------------------------------------------------------------//
-//---- Main algo -----
+//---- Comandline Parameters -----
 //-----------------------------------------------------------------------------------//
 
-     #include "./../lib/TopiiVolMeshAlgo.hpp"
+ for(int i=0; i<argc-1; i++){
 
+   string argvdummy = argv[i];    	
+
+   if(!strcmp(argv[i], "--out"))
+     *outputfile  = argv[i+1];
+ }
+
+   
+ MPI_Barrier(MPI_COMM_WORLD);
+
+ *inputfile  = "point-cloud-strip"; 
+      
+//-----------------------------------------------------------------------------------//
+//---- Main algo for meshing -----
+//-----------------------------------------------------------------------------------//
+
+ #include "./../lib/TopiiVolMeshAlgo.hpp"
 
 //-----------------------------------------------------------------------------------//
 //---- Time  -----
 //-----------------------------------------------------------------------------------//
 
-   MPI_Barrier(MPI_COMM_WORLD);
-   if(mpirank==0){
-    cout << "                                                               \n"
-         << " *============================================================*\n"
-         << std::fixed << std::setprecision(4) 
-         <<  "  The program finshed in : " << MPI_Wtime()-t1 << " s\n"
-         << " *============================================================*\n"
-         << "                                                               \n";
-    }
+ MPI_Barrier(MPI_COMM_WORLD);
+ if(mpirank==0){
+   cout << "                                                               \n"
+        << " *============================================================*\n"
+        << std::fixed << std::setprecision(4) 
+        <<  "  The program finshed in : " << MPI_Wtime()-t1 << " s\n"
+        << " *============================================================*\n"
+        << "                                                               \n";
+ }
 
+ MPI_Finalize();
 
-    MPI_Finalize();
-
-return 0;
+ return 0;
 }
