8bb7b92d582bbf143a2381dc1be241935e261e37
MAB <afeef.badri@gmail.com>
2020-04-18 13:30:21 +0200

    formatting

--- src/lib/TopiiVolPartAlgo.hpp
+++ src/lib/TopiiVolPartAlgo.hpp
@@ -22,87 +22,93 @@
 // ------------- Algo for top-ii-vol part ------------------
 //=============================================================================
 
-  //-----------------------------------------//
-  //---- Program variables -----
-  //-----------------------------------------//
-    int fileNo  = 0 ;   
-    int counter = 0 ;
+//-----------------------------------------//
+//---- Program variables -----
+//-----------------------------------------//
+
+ int fileNo  = 0 ;   
+ int counter = 0 ;
    
-  //-----------------------------------------//
-  //---- Open input & output files -----
-  //-----------------------------------------// 
-    ifstream in  ; in.open(*inputfile+".xyz");
-    ofstream wr  ; wr.open(*outputfile+"_"+std::to_string(fileNo)+".xyz");
-    ofstream wr1 ; wr1.open(*outputfile+"_"+std::to_string(fileNo)+".info"); 
+//-----------------------------------------//
+//---- Open input & output files -----
+//-----------------------------------------// 
+
+ ifstream in  ; 
+   in.open(*inputfile+".xyz");
+
+ ofstream wr  ; 
+   wr.open(*outputfile+"_"+std::to_string(fileNo)+".xyz");
+
+ ofstream wr1 ; 
+   wr1.open(*outputfile+"_"+std::to_string(fileNo)+".info"); 
 
 //=============================================================================
 // ------------- Commandline output ------------------
 //=============================================================================
 
-    cout << "                                                               \n"
-         << "  # points in the .xyz ------------- "<< pntx*pnty << "        \n" 
-         << "  # points in X -------------------- "<< pntx << "             \n"
-         << "  # points in Y -------------------- "<< pnty << "             \n"
-         << "  # of strips ---------------------- "<< mpisize << "          \n"
-         << "                                                               \n"
-         << " *============================================================*\n"
-         << "                                                               \n"
-         << "  top-ii-vol is now stripping the point cloud .......          \n"
-         << "                                                               \n"
-         << "  writing "<<string(*outputfile+"_"+std::to_string(fileNo)+".xyz")<<" ... ";
-
+ cout << "                                                               \n"
+      << "  # points in the .xyz ------------- "<< pntx*pnty << "        \n" 
+      << "  # points in X -------------------- "<< pntx << "             \n"
+      << "  # points in Y -------------------- "<< pnty << "             \n"
+      << "  # of strips ---------------------- "<< mpisize << "          \n"
+      << "                                                               \n"
+      << " *============================================================*\n"
+      << "                                                               \n"
+      << "  top-ii-vol is now stripping the point cloud .......          \n"
+      << "                                                               \n"
+      << "  writing "
+      <<string(*outputfile+"_"+std::to_string(fileNo)+".xyz")<<" ... "    ;
 
 //=============================================================================
 // ------------- Main loop to read file and return the strips  ---------
 //=========================================================================== 
 
-    double* xx = new double[pntx]; 
-    double* yy = new double[pntx];  
-    double* zz = new double[pntx];   
-
-    //double xx[pntx] ;   
-    //double yy[pntx] ;
-    //double zz[pntx] ;
+ double* xx = new double[pntx]; 
+ double* yy = new double[pntx];  
+ double* zz = new double[pntx];   
  
-    int cutoff = pnty/mpisize;
+ int cutoff = pnty/mpisize;
 
-    for(int j=1; j<=pnty; j++){
+ for(int j=1; j<=pnty; j++){
 
-       counter++;
+   counter++;
 
-       for(int i=1; i<=pntx; i++){
-           in >> std::fixed >> xx[i-1]         >> yy[i-1]         >> zz[i-1];
-           wr << std::fixed << xx[i-1] << "\t" << yy[i-1] << "\t" << zz[i-1] << "\n";
-       }
+   for(int i=1; i<=pntx; i++){
+     in >> std::fixed >> xx[i-1]         >> yy[i-1]         >> zz[i-1];
+     wr << std::fixed << xx[i-1] << "\t" << yy[i-1] << "\t" << zz[i-1] << "\n";
+   }
 
-       if(j%cutoff==0 && fileNo < mpisize-1){
+   if(j%cutoff==0 && fileNo < mpisize-1){
 
-          fileNo++; 
-          wr.close(); 
-          cout << "  done\n";
-          cout << "  writing "<<string(*outputfile+"_"+std::to_string(fileNo)+".xyz")<<" ... ";
-          wr.open(*outputfile+"_"+std::to_string(fileNo)+".xyz");
+     fileNo++; 
+     wr.close(); 
+     cout << "  done\n";
+     cout << "  writing "<<string(*outputfile+"_"+std::to_string(fileNo)+".xyz")<<" ... ";
+     wr.open(*outputfile+"_"+std::to_string(fileNo)+".xyz");
 
-          wr1 << counter <<  "  " <<  pntx << "\n"; 
-          counter=0;
+     wr1 << counter <<  "  " <<  pntx << "\n"; 
+     counter=0;
  
-           wr1.close(); wr1.open(*outputfile+"_"+std::to_string(fileNo)+".info");
-          counter++;
+     wr1.close(); wr1.open(*outputfile+"_"+std::to_string(fileNo)+".info");
+     counter++;
 
-          for(int i=1; i<=pntx; i++){
-            wr<< std::fixed << xx[i-1] << "\t" << yy[i-1] << "\t" << zz[i-1] << "\n";
-          }
-        }
-
-    }
-
-    delete[] xx;
-    delete[] yy;
-    delete[] zz;
+     for(int i=1; i<=pntx; i++)
+       wr<< std::fixed << xx[i-1] << "\t" << yy[i-1] << "\t" << zz[i-1] << "\n";
           
-    wr1 << counter <<  "  " <<  pntx;  wr.close(); wr1.close(); in.close(); 
-    cout << "  done\n";
+   }
 
+ }
 
-    cout << "                                                               \n"
-         << " *============================================================*\n";
+ delete[] xx;
+ delete[] yy;
+ delete[] zz;
+          
+ wr1 << counter <<  "  " <<  pntx;  
+
+ in.close(); 
+ wr.close(); 
+ wr1.close(); 
+ 
+ cout << "  done\n";
+
+ cout << "\n *============================================================*\n";
