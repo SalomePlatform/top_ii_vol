9efcb2c07b6da7f4cbbc87b484b4ce0e655f0772
afeef_zia <afeef.badri@gmail.com>
2020-04-20 20:25:17 +0200

    ready code

--- etc/Paralle-mesh-scatter.c
+++ etc/Paralle-mesh-scatter.c
@@ -18,71 +18,35 @@
 
 *******************************************************************************/
 
-
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #include <mpi.h>
 
-float **alloc2d(int n, int m) {
-    float *data = malloc(n*m*sizeof(float));
-    float **array = malloc(n*sizeof(float *));
-    for (int i=0; i<n; i++)
-        array[i] = &(data[i*m]);
-    return array;
-}
+
+int fetchLocalRows(int mpirank, int mpisize, int multipyFactor, int pntM);
+int fetchStartRows(int mpirank, int mpisize, int multipyFactor, int pntM);
 
 int main(int argc, char **argv) {
-
-    double t1 = MPI_Wtime();
 
 //-----------------------------------------------------------------------------------//
 //---- Global Variables -----
 //-----------------------------------------------------------------------------------//
 
+    char filepath[256];
+
     int ierr 	 ;
-    int rank 	 ;
-    int size 	 ;
-
+    int mpirank	 ;
+    int mpisize  ;
     int NPnt     ;
-    int NTri     ;
-    int NTet     ;
-    int label 	 ;
-
-    int	IJK	     ;
-    int	Ip1JK	 ;
-    int	IJp1K	 ;
-    int	IJKp1	 ;
-    int	Ip1JKp1	 ;
-    int	IJp1Kp1	 ;
-    int	Ip1Jp1K	 ;
-    int	Ip1Jp1Kp1;
-    int dummycount;
-
     int startrow ;
     int endrow   ;
     int locnrows ;
-    int locNPnt  ;
     int nrows    ;
 
     float **data ;
 
-    float xx	 ;
-    float yy	 ;
-    float zz	 ;
-    float delz	 ;
-    float zznew  ;
-
     const int charspernum = 41 ;
-
-    char *const fmt1      = "%-13d "     ;
-    char *const endfmt1   = "%-13d\n"    ;
-    char *const fmt       = "%-13.6f   " ;
-    char *const endfmt    = "%-13d\n"    ;
-    char *const fmtint    = "%-11d "     ;
-    char *const endfmtint = "%-7d\n"     ;
-
-    FILE *infile;
 
 //-----------------------------------------------------------------------------------//
 //---- MPI variables -----
@@ -94,54 +58,46 @@ int main(int argc, char **argv) {
     MPI_Status 	 status		;
     MPI_Datatype num_as_string	;
     MPI_Datatype localarray	;
+    MPI_Request request;
 
     ierr = MPI_Init(&argc, &argv);
-    ierr|= MPI_Comm_size(MPI_COMM_WORLD, &size);
-    ierr|= MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    ierr|= MPI_Comm_size(MPI_COMM_WORLD, &mpisize);
+    ierr|= MPI_Comm_rank(MPI_COMM_WORLD, &mpirank);
 
     MPI_Type_contiguous(charspernum, MPI_CHAR, &num_as_string); 
     MPI_Type_commit(&num_as_string);
+
+    double t1 = MPI_Wtime();
 
 //-----------------------------------------------------------------------------------//
 //---- Global Parameters -----
 //-----------------------------------------------------------------------------------//
 
-    double zmax = -1920.0;
-
-    int pntx=120;
-    int pnty=113;
-    int	pntz=51;
+    int pntx=32;
+    int pnty=29;
 
 //-----------------------------------------------------------------------------------//
 //---- Calculating Parameters -----
 //-----------------------------------------------------------------------------------//
 
     NPnt = pntx * pnty						      ;
-    NTri = 4*((pntz-1)*(pntx-1)+(pnty-1)*(pntz-1)+(pntx-1)*(pnty-1))  ;
-    NTet = (pntx-1) * (pnty-1) * (pntz-1) * 6			      ;
-    locNPnt = 	(pntx * pnty)/size ;
 
 //=============================================================================
 // ------------- Commandline logo output ------------------
 //=============================================================================
 
-    if(rank==0){
+    if(mpirank==0){
      #include "./../lib/LogoTopiiVolC.h"
     }
-
 
 //-----------------------------------------------------------------------------------//
 //---- Data allocation -----
 //-----------------------------------------------------------------------------------//
 
-    nrows = NPnt                   ;
-    locnrows = NPnt/size;
-    startrow = rank * locnrows;
-    endrow = startrow + locnrows - 1	;
-    if (rank == size-1) {
-        endrow = nrows - 1;
-        locnrows = endrow - startrow + 1;
-    }
+    locnrows = fetchLocalRows( mpirank, mpisize, 1, NPnt );
+    startrow = fetchStartRows( mpirank, mpisize, 1, NPnt );
+
+    printf("mpirank %d localrow %d startrow %d\n",mpirank,locnrows,startrow);
 
 //-----------------------------------------------------------------------------------//
 //---- create a type describing our piece of the array (Points) -----
@@ -153,55 +109,103 @@ int main(int argc, char **argv) {
     int starts[2]      = {startrow, 0};
     int order          = MPI_ORDER_C  ;
 
-    MPI_Type_create_subarray(2, globalsizes, localsizes, starts, order, num_as_string, &localarray);
+    MPI_Type_create_subarray(2, globalsizes, localsizes, 
+                             starts, order, num_as_string, 
+                             &localarray);
+
     MPI_Type_commit(&localarray);
 
 //-----------------------------------------------------------------------------------//
 //---- open the file, and set the view -----
 //-----------------------------------------------------------------------------------//
 
-    MPI_File_open(MPI_COMM_WORLD, "CoarseMesh-Skip500.xyz", 
-                  MPI_MODE_RDONLY,
-                  MPI_INFO_NULL, &file);
+    MPI_File_open( MPI_COMM_WORLD, "./../../data/DEM_160m.xyz", 
+                   MPI_MODE_RDONLY,
+                   MPI_INFO_NULL, &file);
 
-    MPI_File_open(MPI_COMM_WORLD, "test-wr.xyz", 
-                  MPI_MODE_CREATE|MPI_MODE_WRONLY,
-                  MPI_INFO_NULL, &file1);
+
+    snprintf (filepath, sizeof(filepath), "test-xxxwr_%d.xyz",  mpirank);
+
+    MPI_File_open( MPI_COMM_SELF, filepath, 
+                   MPI_MODE_CREATE | MPI_MODE_RDWR,
+                   MPI_INFO_NULL, &file1 );
 
 //-----------------------------------------------------------------------------------//
-//---- Header writing -----
+//---- Set file view -----
 //-----------------------------------------------------------------------------------//
-
-
-    if(rank==0)printf("\n Writing mesh points ");
-
 
     offset = 0;
-    MPI_File_set_view(file, offset,  MPI_CHAR, localarray, 
-                           "native", MPI_INFO_NULL);
 
-    MPI_File_set_view(file1, offset,  MPI_CHAR, localarray, 
-                           "native", MPI_INFO_NULL);
+    MPI_File_set_view( file, offset,  MPI_CHAR, localarray, 
+                           "native", MPI_INFO_NULL );
 
 //-----------------------------------------------------------------------------------//
 //---- Pointdata writing -----
 //-----------------------------------------------------------------------------------//
+
     char *data_as_txt = malloc(locnrows*1*charspernum*sizeof(char));
 
-    MPI_File_read_all(file, data_as_txt, locnrows*1, num_as_string, &status);
+    MPI_File_iread( file, data_as_txt, locnrows*1, num_as_string,  &request ); 
+    MPI_Wait( &request, &status );
 
-    MPI_File_write_all(file1, data_as_txt, locnrows*1, num_as_string, &status);
+    MPI_File_iwrite( file1, data_as_txt, locnrows*1, num_as_string, &request );
+    MPI_Wait( &request, &status );
+
+//-----------------------------------------------------------------------------------//
+//---- Freeup memory -----
+//-----------------------------------------------------------------------------------//
+
     free(data_as_txt);
     MPI_Type_free(&localarray);
-
-    MPI_Barrier(MPI_COMM_WORLD);
-
-    if(rank==0)
-	printf(" ---- Done\n");
-
     MPI_File_close(&file);
+    MPI_File_close(&file1);
     MPI_Type_free(&num_as_string);
 
     MPI_Finalize();
     return 0;
+}
+
+
+//====================================================================================//
+//---- function to set localrow -----
+//====================================================================================//
+
+int fetchLocalRows(int mpirank, int mpisize, int multipyFactor, int pntM)
+{
+
+    int locnrows;
+    int Remainder = pntM%mpisize ;
+    int Quotient  = pntM/mpisize ;
+
+    if( Remainder == 0 )
+        locnrows = multipyFactor * Quotient ;
+    else if( mpirank < Remainder )
+        locnrows = multipyFactor * (Quotient+1);
+    else
+        locnrows = multipyFactor * Quotient;
+
+    return locnrows;
+
+}
+
+//====================================================================================//
+//---- function to set startrow -----
+//====================================================================================/
+
+int fetchStartRows(int mpirank, int mpisize, int multipyFactor, int pntM)
+{
+
+    int startrow;
+    int Remainder = pntM%mpisize ;
+    int Quotient  = pntM/mpisize ;
+
+    if(Remainder == 0)
+        startrow = mpirank * multipyFactor * Quotient;
+    else if(mpirank < Remainder)
+        startrow = mpirank * multipyFactor * (Quotient + 1)   ;
+    else
+        startrow = mpirank * multipyFactor * Quotient + Remainder * multipyFactor    ;
+
+    return startrow;
+
 }
